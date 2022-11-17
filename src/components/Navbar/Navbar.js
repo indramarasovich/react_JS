39cfc1839353ec3dcf40109d3921893a872c544f
Indra Marasovich <indramarasovich@gmail.com>
2022-11-17 16:41:21 -0300

    second

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>MyApp</h1>
+            <button className='boton'> boton 1 </button>        
+        </nav>
+    )
+}
+
+export default Navbar
